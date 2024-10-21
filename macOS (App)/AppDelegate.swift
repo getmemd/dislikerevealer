9c4bb5b0ea832ef806f2b251c4b96b9950f96063
adil <amedeuyev@gmail.com>
2024-10-21 06:16:23 +0500

    userscript and multiplatform app

---
+++ macOS (App)/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  macOS (App)
+//
+//  Created by Adilkhan Medeuyev on 16.10.2024.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+    
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+}
